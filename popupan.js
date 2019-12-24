91598c5f387917e032f9cf5e220f8c0291e9e582
phamtruongthanh <59016393+phamtruongthanh@users.noreply.github.com>
2019-12-24 16:12:47 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'https://phamtruongthanh.blogspot.com/';
+	var url_popup = 'https://xekia093372105.blogspot.com/';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
